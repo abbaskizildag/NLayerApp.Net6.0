c8c683771b92761dcde048977f772f2f1d8c05d0
Ali Kızıldağ <abbaskizildag@gmail.com>
2022-10-01 15:53:37 +0300

    added Interfaces

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll(Expression<Func<T,bool>> expression);
+        Task<T> GetByIdAsync(int id);
+
+        // productRepository.Where(x=>x.id>5).ToListAsync();
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
