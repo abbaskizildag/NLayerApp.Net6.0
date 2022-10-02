e5693fc6560e0fe0add98b37e54b620926bf3ccf
Ali Kızıldağ <abbaskizildag@gmail.com>
2022-10-02 09:05:24 +0300

    added custom endpoint and service

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -14,10 +14,19 @@ namespace NLayer.API.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IService<Product> _service;
-        public ProductsController(IMapper mapper, IService<Product> service)
+        private readonly IProductService _productService;
+        public ProductsController(IMapper mapper, IService<Product> service, IProductService productService)
         {
             _mapper = mapper;
             _service = service;
+            _productService = productService;
+        }
+
+        //GET Api/products/GetProductsWithCategory
+        [HttpGet("[action]")] //controller'da iki tane httpget olduğu için bunu belirttim.
+        public async Task<IActionResult> GetProductsWithCategory()
+        {
+            return CreateActionResult(await _productService.GetProductsWithCategory());
         }
 
         [HttpGet]
